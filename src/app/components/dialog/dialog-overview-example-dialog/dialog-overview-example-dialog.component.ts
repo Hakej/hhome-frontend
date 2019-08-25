5b8291000a2d317392d44f80c4ae926746336525
Hakej <michaluszak97@gmail.com>
2019-08-25 22:02:32 +0200

    Add dialog component, routing doesn't quite work

---
+++ src/app/components/dialog/dialog-overview-example-dialog/dialog-overview-example-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { DialogData } from '../dialog.component';
+
+@Component({
+  selector: 'app-dialog-overview-example-dialog',
+  templateUrl: './dialog-overview-example-dialog.component.html',
+  styleUrls: ['./dialog-overview-example-dialog.component.scss']
+})
+export class DialogOverviewExampleDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { DialogOverviewExampleDialogComponent } from './dialog-overview-example-dialog/dialog-overview-example-dialog.component';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent {
+
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogComponent, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+}
